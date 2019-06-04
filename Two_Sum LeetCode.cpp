dc67efc7ab393a0d812452d3a71c3dac80a1d96c
Sidharth Das <sidharth2189@gmail.com>
2019-06-04 16:30:40 +0530

    Update Two_Sum LeetCode.cpp
    Time complexity reduced

--- Two_Sum LeetCode.cpp
+++ Two_Sum LeetCode.cpp
@@ -20,21 +20,48 @@ public:
     
     vector<int> twoSum(vector<int>& nums, int target) 
     {
-        int i, j, sum ;
-        //vector<int> index;
-        for (i = 0; i < sizeof(nums); i++)
+        int left_index, right_index;
+        
+        // Size of array
+        int n;
+        n = sizeof(nums)/(sizeof(nums[0]));
+
+        /* step -1) Sort the array in ascending order*/
+        sort(nums.begin(), nums.begin()+n);
+        
+        /* Step -2) Initialize two index variables to find the candidate 
+            elements in the sorted array.
+            
+            (a) Initialize first to the leftmost index: l = 0
+            (b) Initialize second  the rightmost index:  r = artray_size-1 */
+        
+        left_index = 0;
+        right_index = n - 1;
+
+        /* Step -3) Loop while left_index < right_index.
+            (a) If (Array[left_index] + Array[right_index] == sum)  then return 1
+            (b) Else if( Array[left_index] + Array[right_index] <  sum )  then left_index++
+            (c) Else right_index-- */
+        
+        while (left_index < right_index)
         {
-            for (j = i + 1; j < sizeof(nums); j++)
+            if (nums[left_index] + nums[right_index] == target)
             {
-                sum = nums[i] + nums[j];
-                if (sum == target)
-                {
-                    index.push_back(i);
-                    index.push_back(j);
-                    return index;
-                }   
+                index.push_back(left_index);
+                index.push_back(right_index);
+                break;
+            }
+            else if (nums[left_index] + nums[right_index] < target)
+            {
+                left_index++;
+            }
+            else
+            {
+                right_index--;
             }
         }
+
+        /* Step -4) If no candidates in whole array - return 0 */
         return index;
     }
 };
